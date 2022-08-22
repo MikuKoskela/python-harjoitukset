3ad85b132ee00508fcc1b8ccc24980a0c59d7413
Miku Koskela <Miku.Koskela@metropolia.fi>
2022-08-22 14:45:36 +0300

    Tehtävä 2.3

---
+++ Tehtävä 2.3.py
@@ -0,0 +1,16 @@
+
+import math
+
+kanta = float(input("Anna suorakulmion kanta "))
+korkeus = float(input("Anna suorakulman korkeus"))
+
+A = kanta * korkeus
+piiri = (kanta * 2) + (korkeus * 2)
+
+print(f"Ala = {A}")
+print(f"piiri = {piiri}")
+
+
+
+
+
