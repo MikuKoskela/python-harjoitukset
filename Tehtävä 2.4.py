576d4320b8fc47eaa04bea08abaeab6fcbc6b119
Miku Koskela <Miku.Koskela@metropolia.fi>
2022-08-22 14:58:02 +0300

    Tehtävä 2.4

---
+++ Tehtävä 2.4.py
@@ -0,0 +1,17 @@
+#Syötteet
+
+luku_1 = int(input("Anna 1.kokonaisluku"))
+luku_2 = int(input("Anna 2.kokonaisluku"))
+luku_3 = int(input("Anna 3.kokonaisluku"))
+
+#laskutoimitukset
+
+summa = luku_1 + luku_2 + luku_3
+tulo = luku_1 * luku_2 * luku_3
+ka = summa / 3
+
+#tulostus
+
+print(summa)
+print(tulo)
+print(ka)
